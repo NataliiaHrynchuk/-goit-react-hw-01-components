b0cbc2d958ea399945337a65257d51e65bad8933
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-08-18 17:54:30 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { Profile } from "components/Profile/Profile";
 import user from "components/Profile/user.json";
+import data from 'components/Statistics/data.json';
+import friends from "components/FriendList/friends.json";
 
-
+import { Profile } from "components/Profile/Profile";
+import {Statistics} from 'components/Statistics/Statistics';
+import { FriendList } from "components/FriendList/FriendList";
 export const App = () => {
   return (
     <div>
       <Profile user={user}/>
+      <Statistics title='Upload stats'
+      stats={data}/>
+      <FriendList items={friends}/>
     </div>
   );
 };
