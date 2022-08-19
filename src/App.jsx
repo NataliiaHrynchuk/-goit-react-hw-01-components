2bc2f19bb3a5457b479a6a511f82ab889e51c909
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-08-19 12:38:59 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import user from "components/Profile/user.json";
 import data from 'components/Statistics/data.json';
 import friends from "components/FriendList/friends.json";
+import transactions from "components/TransactionHistory/transactions.json";
 
 import { Profile } from "components/Profile/Profile";
 import {Statistics} from 'components/Statistics/Statistics';
 import { FriendList } from "components/FriendList/FriendList";
+import { TransactionHistory } from "components/TransactionHistory/TransactionHistory";
+
 export const App = () => {
   return (
     <div>
@@ -12,6 +15,7 @@ export const App = () => {
       <Statistics title='Upload stats'
       stats={data}/>
       <FriendList items={friends}/>
+      <TransactionHistory items={transactions}/>
     </div>
   );
 };
