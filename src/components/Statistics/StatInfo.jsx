f29b96197800bd7815a0b152df62b3c43d5ca465
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-08-20 22:22:37 +0300

    Update StatInfo.jsx

--- src/components/Statistics/StatInfo.jsx
+++ src/components/Statistics/StatInfo.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
+import css from 'components/Statistics/StatInfo.module.css';
 export const StatInfo = ({label, percentage}) => {
-    return <div>
-        <span>{label}</span>
-        <span>{percentage}%</span>
-    </div>
-}
+    return( <div className={css.stat_info}>
+        <span className={css.label}>{label}</span>
+        <span className={css.percentage}>{percentage}%</span>
+    </div>);
+};
 
 StatInfo.propTypes = {
     label: PropTypes.string.isRequired,
