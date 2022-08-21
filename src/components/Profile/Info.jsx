b0fa8205b4c4af813705d6903b0b417d90a004ad
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-08-21 22:00:37 +0300

    Update Info.jsx

--- src/components/Profile/Info.jsx
+++ src/components/Profile/Info.jsx
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types';
+import css from 'components/Profile/Info.module.css';
 export const Info = ({label, quantity }) => {
-    return <li>
-      <span>{label}</span>
-      <span>{quantity}</span>
+    return <li className={css.stats_item}>
+      <span className={css.label}>{label}</span>
+      <span className={css.quantity}>{quantity}</span>
     </li>
   }
 
