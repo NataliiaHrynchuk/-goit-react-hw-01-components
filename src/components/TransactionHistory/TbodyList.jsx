167b5b8da4aa7d1411ffd0047c626e9e204181d0
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-08-22 19:36:11 +0300

    Update TbodyList.jsx

--- src/components/TransactionHistory/TbodyList.jsx
+++ src/components/TransactionHistory/TbodyList.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
+import css from 'components/TransactionHistory/TbodyList.module.css';
 export const TbodyList = ({items}) => {
-    return <tbody>
+    return <tbody className={css.tbody}>
         {items.map(item => (
-            <tr key={item.id}>
-                <td>{item.type}</td>
-                <td>{item.amount}</td>
-                <td>{item.currency}</td>
+            <tr key={item.id} className={css.tr}>
+                <td className={css.td}>{item.type}</td>
+                <td className={css.td}>{item.amount}</td>
+                <td className={css.td}>{item.currency}</td>
             </tr>
         ))}
 
