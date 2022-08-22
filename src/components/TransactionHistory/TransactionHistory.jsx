c98fc92a2bb941a8704629545bff6e565cf60984
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-08-22 19:36:24 +0300

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types';
 import { TbodyList } from "./TbodyList";
 import { Thead } from "./Thead";
+import css from 'components/TransactionHistory/TransactionHistory.module.css';
 export const TransactionHistory = ({items}) => {
-    return <table>
+    return <table className={css.transaction_history}>
             <Thead nameFirstColumn='Type'
                nameSecondColumn='Amount'
                nameThirdColumn='Currency'/>
