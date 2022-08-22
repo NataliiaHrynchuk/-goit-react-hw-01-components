77b782b1e6f7d58cbfecbcf4971d2903aa64353a
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-08-22 19:35:46 +0300

    Update StatInfo.jsx

--- src/components/Statistics/StatInfo.jsx
+++ src/components/Statistics/StatInfo.jsx
@@ -1,11 +1,8 @@
 import PropTypes from 'prop-types';
 import css from 'components/Statistics/StatInfo.module.css';
-import {getRandomHexColor} from 'Utils/getRandomHexColor';
-
 
 export const StatInfo = ({label, percentage}) => {
-    const bgColor = getRandomHexColor();
-        return( <div className={css.stat_info} background-color={bgColor}>
+    return( <div className={css.stat_info}>
         <span className={css.label}>{label}</span>
         <span className={css.percentage}>{percentage}%</span>
     </div>);
