09d6f78180f3a91bd043e6c57ec61a3925a1980d
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-08-22 19:36:00 +0300

    Update StatList.jsx

--- src/components/Statistics/StatList.jsx
+++ src/components/Statistics/StatList.jsx
@@ -2,13 +2,14 @@ import PropTypes from 'prop-types';
 import css from 'components/Statistics/StatList.module.css';
 import { StatInfo } from 'components/Statistics/StatInfo';
 import {getRandomHexColor} from 'Utils/getRandomHexColor';
-
 const bgColor = getRandomHexColor();
+console.log(bgColor);
+
 export const StatList = ({items}) => {
+   
 return (<ul className={css.stat_list}>
     {items.map(item => ( 
-       
-        < li key={item.id} background-color={bgColor}>
+        <li key={item.id} className={css.item} style={{backgroundColor:getRandomHexColor()}}>
             <StatInfo
                label={item.label}
                percentage={item.percentage}/>
