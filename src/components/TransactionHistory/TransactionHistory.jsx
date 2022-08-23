621f901f325774758c9d703524c090a865683477
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-08-23 13:22:12 +0300

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,17 +1,15 @@
 import PropTypes from 'prop-types';
 import { TbodyList } from "./TbodyList";
 import { Thead } from "./Thead";
-import css from 'components/TransactionHistory/TransactionHistory.module.css';
+import { Table } from './TransactionHistory.styled';
+
 export const TransactionHistory = ({items}) => {
-    return <table className={css.transaction_history}>
-            <Thead nameFirstColumn='Type'
-               nameSecondColumn='Amount'
-               nameThirdColumn='Currency'/>
-               
-            <TbodyList items={items}/>
-            
-            
-    </table>
+    return <Table>
+                <Thead nameFirstColumn='Type'
+                    nameSecondColumn='Amount'
+                    nameThirdColumn='Currency'/>
+                <TbodyList items={items}/>
+            </Table>
 }
 TransactionHistory.propTypes = {
     items: PropTypes.arrayOf(
