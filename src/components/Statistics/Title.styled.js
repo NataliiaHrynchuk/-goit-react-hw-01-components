19eb7c2747573a3131c750bc9f18a98b9c8d8a1a
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-08-23 12:23:04 +0300

    Create Title.styled.js

---
+++ src/components/Statistics/Title.styled.js
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const Heading = styled.h2`
+margin-block-start: 0;
+    margin-block-end: 0;
+    padding: 40px;
+    text-align: center;
+    text-transform: uppercase;
+    color: rgb(82, 80, 80);
+    background-color: white;
+    border-top-left-radius: 4px;
+    border-top-right-radius: 4px;
+`;
