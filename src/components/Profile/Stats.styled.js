97ef5429c78887aeeaa3420a1ced45f0ffc44363
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-08-23 11:23:26 +0300

    Create Stats.styled.js

---
+++ src/components/Profile/Stats.styled.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const StatsList = styled.ul`
+width: 100%;
+    display: flex;
+    list-style: none;
+    margin-block-start: 0;
+    margin-block-end: 0;
+    padding-inline-start: 0;
+`;
