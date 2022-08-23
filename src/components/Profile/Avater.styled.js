02d52c2769cd40eacafcfd23aa8ed0d504ea4042
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-08-23 11:22:58 +0300

    Create Avater.styled.js

---
+++ src/components/Profile/Avater.styled.js
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled';
+export const Description = styled.div`
+background-color: white;
+  padding: 20px;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+  border-bottom: 0.1px rgb(214, 212, 212) solid;
+`;
+
+export const ImageUrl = styled.img`
+width: 200px;
+  height: 200px;
+  border-radius: 50%;
+`;
+export const Name = styled.p`
+font-size: 24px;
+  font-weight: 600;
+`;
+export const Tag = styled.p`
+font-weight: 600;
+   color: grey;
+`;
+export const Location = styled.p`
+font-weight: 600;
+  color: grey;
+`;
