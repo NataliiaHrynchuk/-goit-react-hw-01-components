98f94b12820ff47f99a42f291a8fee56c19a7609
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-08-23 13:22:09 +0300

    Create Thead.styled.js

---
+++ src/components/TransactionHistory/Thead.styled.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const HeaderLine = styled.thead`
+height: 30px;
+  background-color: rgb(35, 240, 240);
+  color: white;
+  text-transform: uppercase;
+  font-size: 14px;
+`;
+
