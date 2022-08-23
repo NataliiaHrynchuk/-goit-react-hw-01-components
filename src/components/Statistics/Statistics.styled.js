44bc9a19322ce350ab7926de1abde43d12d965b7
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-08-23 12:22:48 +0300

    Create Statistics.styled.js

---
+++ src/components/Statistics/Statistics.styled.js
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const StatisticsSection = styled.section`
+    width: 325px;
+    margin-bottom: 50px;
+    margin-left: auto;
+    margin-right: auto;
+`;
